54d526583e736a33bd563d03af02462cd23ed59d
DianaPetrovska <259563350+dpetrovskaua@users.noreply.github.com>
2026-04-29 15:03:54 +0300

    Testing of main.py

--- testing.py
+++ testing.py
@@ -8,17 +8,19 @@ def matrix():
 def test_initialization(matrix):
     assert matrix.length == 0
     assert matrix.A == []
-
-def test_average_calculation(matrix):
-    matrix.C = [[10, 20], [30, 40]]
-    # (10+20+30+40) / 4 = 25
-    assert matrix.get_average() == 25.0
-
+    
 def test_multiply_logic(matrix):
     matrix.length = 2
-    matrix.A = [[1, 1], [1, 1]]
-    matrix.B = [[2, 2], [2, 2]]
-
+    matrix.A = [[1, 1],
+                [1, 1]]
+    matrix.B = [[2, 2],
+                [2, 2]]
     matrix.multiply()
-    expected = [[4, 4], [4, 4]]
-    assert matrix.C == expected
+    assert matrix.C == [[4, 4],
+                        [4, 4]]
+
+def test_average_calculation(matrix):
+    matrix.C = [[10, 20],
+                [30, 40]]
+    # (10+20+30+40) / 4 = 25
+    assert matrix.get_average() == 25.0
